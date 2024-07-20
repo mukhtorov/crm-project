2e070892c3b66893cd86b5e3e136644e79d7bcba
mukhtorov <mukhtorov_1994@mail.ru>
2024-07-20 05:45:24 +0300

    collapse detail finished

--- src/components/Kurslar/GroupsList/Table.jsx
+++ src/components/Kurslar/GroupsList/Table.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 import * as React from "react";
-import PropTypes from "prop-types";
 import Box from "@mui/material/Box";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -11,6 +10,15 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Delete, Edit, Status, TimelineWrapper, Title } from "./tableStyle";
+
+const styleCell = {
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  border: 0,
+  gap: "4px",
+};
 
 function Row(props) {
   const { row } = props;
@@ -30,42 +38,72 @@ function Row(props) {
           scope="row"
           sx={{
             color: "#253E5F",
-            fontWeight: 500,
-            background: "",
-            fontSize: "16px",
+            fontWeight: 600,
+            fontSize: "18px",
             border: 0,
           }}
         >
           {row.title}
         </TableCell>
-        <TableCell sx={{ border: 0 }} />
+        <TableCell sx={{ border: 0 }} align="right">
+          <Edit />
+          <Delete />
+        </TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Location</TableCell>
-                    <TableCell>Status</TableCell>
-                    <TableCell align="right">Schedule</TableCell>
-                    <TableCell align="right">action</TableCell>
-                  </TableRow>
-                </TableHead>
                 <TableBody>
-                  {row?.groups?.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                  {row?.groups?.map((rw) => {
+                    console.log(rw, "rooww");
+                    return (
+                      <TableRow
+                        key={rw.date}
+                        sx={{
+                          display: "flex",
+                          border: "1px solid #d8d8d8",
+                          borderRadius: "8px",
+                          margin: "8px",
+                          padding: "10px",
+                          width: "100%",
+                        }}
+                      >
+                        <TableCell sx={styleCell}>
+                          <Title>{rw.level}</Title>
+                          <Status active={rw.started}>
+                            {rw.started ? "Active" : "Soon"}
+                          </Status>
+                        </TableCell>
+                        <TableCell sx={{ ...styleCell, flex: 2 }}>
+                          <Title>{rw.filial}</Title>
+                          <Title color={"#1890FF"}>{rw.location}</Title>
+                        </TableCell>
+                        <TableCell sx={{ ...styleCell, flex: 2, gap: "8px" }}>
+                          <TimelineWrapper>
+                            {rw.timeline.replace(/ /g, " - ")}
+                          </TimelineWrapper>
+                          <TimelineWrapper time>{rw.time}</TimelineWrapper>
+                        </TableCell>
+                        <TableCell sx={styleCell}>
+                          <Title center>O'qituvchilar</Title>
+                          <Title color={"#929FAF"} center>
+                            +{rw.students.length}
+                          </Title>
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            border: 0,
+                            display: "flex",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Edit />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
                 </TableBody>
               </Table>
             </Box>
@@ -77,7 +115,7 @@ function Row(props) {
 }
 
 export default function CollapsibleTable(props) {
-  const { cells, rows } = props;
+  const { rows } = props;
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
---
+++ src/components/Kurslar/GroupsList/tableStyle.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import edit from "../../../assets/icons/edit.svg?react";
+import del from "../../../assets/icons/delete.svg?react";
+
+export const Edit = styled(edit)`
+  padding: 4px;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+export const Delete = styled(del)`
+  padding: 4px;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+export const Title = styled.div`
+  display: flex;
+  justify-content: ${({ center }) => center && "center"};
+  font-family: Montserrat;
+  font-weight: 500;
+  line-height: 24px;
+  white-space: nowrap;
+  color: ${({ color }) => (color ? color : "#253e5f")};
+  font-size: ${({ size }) => (size ? `${size}px` : "16px")};
+`;
+
+export const Status = styled.div`
+  color: ${({ active }) => (active ? "#52C41A" : "#FAAD14")};
+  font-family: Montserrat;
+  font-size: ${({ size }) => (size ? `${size}px` : "16px")};
+  font-weight: 600;
+  line-height: 20px;
+
+  &::before {
+    display: inline-block;
+    content: " ";
+    width: 8px;
+    height: 8px;
+    background-color: ${({ active }) => (active ? "#52C41A" : "#FAAD14")};
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+`;
+
+export const TimelineWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({ time }) => (time ? "#91D5FF" : "#1890FF")};
+
+  /* width: Fixed (250px) px; */
+  height: 28px;
+  padding: 6px 12px;
+  gap: 8px;
+  border-radius: 16px;
+
+  font-family: Montserrat;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  color: white;
+
+  width: 100%;
+  max-width: 300px;
+`;
