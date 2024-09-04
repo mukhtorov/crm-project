7fa881a90ecf79b1a3f324f37cbd0cd28d24b32f
mukhtorov <mukhtorov_1994@mail.ru>
2024-09-04 18:41:04 +0300

    manger sozlamalar finished

--- src/components/Sozlamalar/Manager/Darajalar/modal.jsx
+++ src/components/Sozlamalar/Manager/Darajalar/modal.jsx
@@ -4,18 +4,50 @@ import { Modal } from "../../../Generics/Modal";
 import GenericInput from "../../../Generics/Input";
 import Subtitle from "../../../Generics/Subtitle";
 import Title from "../../../Generics/Title";
+import { useState } from "react";
+import useFetch from "../../../../hooks/useFetch";
 
+const initialState = {
+  status: "",
+  half: "",
+  full: "",
+};
 export const DarajalarModal = (props) => {
-  const { data } = props;
+  const [state, setState] = useState(initialState);
+  const request = useFetch();
+
+  const onChange = ({ target: { value, name } }) => {
+    setState({ ...state, [name]: value });
+  };
+
+  const onSave = () => {
+    console.log(state, "state");
+
+    request(`/tabs/manager_status`, {
+      method: "POST",
+      body: { ...state, id: Date.now() },
+    }).then(() => {
+      props.reload();
+      alert("added");
+    });
+
+    props.onSave();
+  };
 
   return (
-    <Modal {...props}>
+    <Modal {...props} onSave={onSave}>
       <Title size="34px">Daraja Qo'shish</Title>
       {/* Nome */}
       <Subtitle mt={16} mb={8} color={"#929FAF"}>
         Nomi
       </Subtitle>
-      <GenericInput fontWeight={500} width={500} value={data?.name} />
+      <GenericInput
+        fontWeight={500}
+        width={500}
+        value={state?.status}
+        onChange={onChange}
+        name="status"
+      />
       {/* Manzil*/}
 
       <div style={{ display: "flex", gap: 20 }}>
@@ -23,13 +55,23 @@ export const DarajalarModal = (props) => {
           <Subtitle mt={16} mb={8} color={"#929FAF"}>
             Yarim Stavka
           </Subtitle>
-          <GenericInput fontWeight={500} value={data?.name} />
+          <GenericInput
+            fontWeight={500}
+            value={state?.half}
+            onChange={onChange}
+            name="half"
+          />
         </div>
         <div style={{ flex: 1 }}>
           <Subtitle mt={16} mb={8} color={"#929FAF"}>
             Bir Stavka
           </Subtitle>
-          <GenericInput fontWeight={500} value={data?.name} />
+          <GenericInput
+            fontWeight={500}
+            value={state?.full}
+            onChange={onChange}
+            name="full"
+          />
         </div>
       </div>
     </Modal>
